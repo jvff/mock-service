e6f8366df8c1cb1ddf6424a906ac011244469f43
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-02-13 19:22:19 +0000

    Use request references in MockService trait

--- src/mock_service.rs
+++ src/mock_service.rs
@@ -5,9 +5,9 @@ use super::mock_action::MockAction;
 pub trait MockService: Service {
     fn set_action(
         &mut self,
-        request: <Self as Service>::Request,
+        request: &<Self as Service>::Request,
         action: &mut MockAction<Self>,
     );
 
-    fn remove_action(&mut self, request: <Self as Service>::Request);
+    fn remove_action(&mut self, request: &<Self as Service>::Request);
 }
